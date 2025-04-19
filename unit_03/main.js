b8b75dbdf9ed288fcdf7aa73dd74aae78c0c3a68
Kailash Khadka <k.khadka343@gmail.com>
2025-04-19 01:07:47 +0000

    Vuejs: Connect, move, turn and stop

--- unit_03/main.js
+++ unit_03/main.js
@@ -3,11 +3,11 @@ let vueApp = new Vue({
     data: {
         // ros connection
         ros: null,
-        rosbridge_address: 'wss://i-02ceddab37f42e4f6.robotigniteacademy.com/fb02d677-4756-4158-a71a-e833afe90221/rosbridge/', // change to your own address
+        rosbridge_address: 'wss://i-02ceddab37f42e4f6.robotigniteacademy.com/fb02d677-4756-4158-a71a-e833afe90221/rosbridge/',
         connected: false,
         // page content
         menu_title: 'Connection',
-        main_title: 'ACTIONS',
+        main_title: 'Main title, from Vue!!',
     },
     methods: {
         connect: function() {
@@ -45,8 +45,32 @@ let vueApp = new Vue({
             })
             topic.publish(message)
         },
+        turnRight: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            topic.publish(message)
+        },
+        stop: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            topic.publish(message)
+        },
     },
     mounted() {
-        console.log('Mounted: page is ready!')
+        console.log('page is ready!')
     },
 })
