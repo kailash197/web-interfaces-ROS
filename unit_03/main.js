7f1941ad7134a828f1f9e2b69d1e2372ee63927d
Kailash Khadka <k.khadka343@gmail.com>
2025-04-19 01:04:44 +0000

    Vuejs: Connect rosbridge, move robot

--- unit_03/main.js
+++ unit_03/main.js
@@ -4,30 +4,49 @@ let vueApp = new Vue({
         // ros connection
         ros: null,
         rosbridge_address: 'wss://i-02ceddab37f42e4f6.robotigniteacademy.com/fb02d677-4756-4158-a71a-e833afe90221/rosbridge/', // change to your own address
+        connected: false,
         // page content
-        menu_title: 'My menu title',
-        main_title: 'Main title, from Vue!!',
-        p_content_01: 'This is the left side of my web page. It occupies 33% of the total width',
-        p_content_02: 'Here it goes the main content of my first web page, able to control ROS robots using web tools!',
+        menu_title: 'Connection',
+        main_title: 'ACTIONS',
+    },
+    methods: {
+        connect: function() {
+            // define ROSBridge connection object
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+
+            // define callbacks
+            this.ros.on('connection', () => {
+                this.connected = true
+                console.log('Connection to ROSBridge established!')
+            })
+            this.ros.on('error', (error) => {
+                console.log('Something went wrong when trying to connect')
+                console.log(error)
+            })
+            this.ros.on('close', () => {
+                this.connected = false
+                console.log('Connection to ROSBridge was closed!')
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        },
     },
     mounted() {
         console.log('Mounted: page is ready!')
-
-        // define ROSBridge connection object
-        this.ros = new ROSLIB.Ros({
-            url: this.rosbridge_address
-        })
-
-        // define callbacks
-        this.ros.on('connection', () => {
-            console.log('Connection to ROSBridge established!')
-        })
-        this.ros.on('error', (error) => {
-            console.log('Something went wrong when trying to connect')
-            console.log(error)
-        })
-        this.ros.on('close', () => {
-            console.log('Connection to ROSBridge was closed!')
-        })
     },
 })
