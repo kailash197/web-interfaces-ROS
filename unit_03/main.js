bf4de7d2bcd67e8cf1db113c146f278a8b336df2
Kailash Khadka <k.khadka343@gmail.com>
2025-04-19 00:28:00 +0000

    Vue: mount() func

--- unit_03/main.js
+++ unit_03/main.js
@@ -5,5 +5,8 @@ let vueApp = new Vue({
         main_title: 'Main title, from Vue!!',
         p_content_01: 'This is the left side of my web page. It occupies 33% of the total width',
         p_content_02: 'Here it goes the main content of my first web page, able to control ROS robots using web tools!',
+    },
+    mounted() {
+        console.log('Mounted: page is ready!')
     }
 })
