c09564ae9fa7828be8bde20d88db8f37226c8604
Kailash Khadka <k.khadka343@gmail.com>
2025-04-19 02:11:33 +0000

    Start same as unit03

---
+++ unit_03_extra/main.js
@@ -0,0 +1,76 @@
+let vueApp = new Vue({
+    el: "#vueApp",
+    data: {
+        // ros connection
+        ros: null,
+        rosbridge_address: 'wss://i-06f04653f08c9ea7c.robotigniteacademy.com/56c208a0-832b-4e10-a5f5-fc4523b103e3/rosbridge/',
+        connected: false,
+        // page content
+        menu_title: 'Connection',
+        main_title: 'Main title, from Vue!!',
+    },
+    methods: {
+        connect: function() {
+            // define ROSBridge connection object
+            this.ros = new ROSLIB.Ros({
+                url: this.rosbridge_address
+            })
+
+            // define callbacks
+            this.ros.on('connection', () => {
+                this.connected = true
+                console.log('Connection to ROSBridge established!')
+            })
+            this.ros.on('error', (error) => {
+                console.log('Something went wrong when trying to connect')
+                console.log(error)
+            })
+            this.ros.on('close', () => {
+                this.connected = false
+                console.log('Connection to ROSBridge was closed!')
+            })
+        },
+        disconnect: function() {
+            this.ros.close()
+        },
+        sendCommand: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0.5, },
+            })
+            topic.publish(message)
+        },
+        turnRight: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            topic.publish(message)
+        },
+        stop: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            topic.publish(message)
+        },
+    },
+    mounted() {
+        console.log('page is ready!')
+    },
+})
